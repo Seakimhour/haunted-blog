c76d4f16a97211f515de06159d2ade17f2b6936a
Sea Kimhour <seakimhour@gmail.com>
2022-11-11 17:26:11 +0900

    Route脆弱性対策

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,7 @@ class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   before_action :set_blog, only: %i[show edit update destroy]
+  before_action :owner_validate, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -42,6 +43,10 @@ class BlogsController < ApplicationController
   end
 
   private
+
+  def owner_validate
+    redirect_to blogs_url if !@blog.owned_by?(current_user)
+  end
 
   def set_blog
     @blog = Blog.find(params[:id])
